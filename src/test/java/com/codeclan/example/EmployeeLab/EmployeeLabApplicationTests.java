86701dfb02f4856b4705ceaf023ec3df45fb125d
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-30 14:24:17 +0000

    tests failed - nmeed to debug - error message not clear

--- src/test/java/com/codeclan/example/EmployeeLab/EmployeeLabApplicationTests.java
+++ src/test/java/com/codeclan/example/EmployeeLab/EmployeeLabApplicationTests.java
@@ -1,13 +1,24 @@
 package com.codeclan.example.EmployeeLab;
 
+import com.codeclan.example.EmployeeLab.models.Employee;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class EmployeeLabApplicationTests {
 
+	@Autowired
+	Employee employee;
+
 	@Test
 	void contextLoads() {
+
+		Employee json = new Employee("Json", "Jsonson", 101, "6412242", "json@jsonson.com");
+		Employee graham = new Employee("Graham", "Graemson", 29, "876152638", "Graeme@grahamson.com");
 	}
 
 }
